5ab2a38b252b7ca9e1e7dc2bc4d5ec2409a1aa70
Raimon <raimon.txollo@gmail.com>
2025-04-29 13:45:40 +0200

    Add test for health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import startServer from "./server/startServer.js";
 import connectToDatabase from "./database/connectToDatabase.js";
 
 const port = process.env.PORT ?? 3001;
+const databaseConnectionString = process.env.DATABASE_CONNECTION_STRING;
 
-await connectToDatabase(
-  "mongodb+srv://raimontxollo:negri6000@cluster0.hojql6o.mongodb.net/blog2",
-);
+await connectToDatabase(databaseConnectionString);
 startServer(port);
