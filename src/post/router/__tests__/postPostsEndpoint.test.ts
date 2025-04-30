73cc6b5e121b6f5af411460e550b1817e5b7e349
Raimon <raimon.txollo@gmail.com>
2025-04-30 23:24:51 +0200

    Add tests for POST /posts endpoint

---
+++ src/post/router/__tests__/postPostsEndpoint.test.ts
@@ -0,0 +1,51 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import Post from "../../model/Post.js";
+import { callos, conejo, pajaritos } from "../../fixtures.js";
+import connectToDatabase from "../../../database/connectToDatabase.js";
+import app from "../../../server/app.js";
+
+let server: MongoMemoryServer;
+
+beforeAll(async () => {
+  server = await MongoMemoryServer.create();
+  const serverUri = server.getUri();
+  await connectToDatabase(serverUri);
+
+  await Post.create(callos, pajaritos);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await server.stop();
+});
+
+describe("Given the POST /posts endpoint", () => {
+  describe("When it receives a request with 'Conejo en escabeche del Bar Amadeo' reciepe", () => {
+    test("Then it should respose with a 201 status code and 'Conejo en escabeche del Bar Amadeo' reciepe", async () => {
+      const expectedStatusCode = 201;
+      const coso = conejo.title;
+
+      const response = await request(app)
+        .post("/posts")
+        .send(conejo)
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json");
+
+      expect(response.status).toBe(expectedStatusCode);
+      expect(response.body.title).toBe(coso);
+    });
+
+    test("Then it should response with a 409 status code and 'Post with this title already exists' error", async () => {
+      const response = await request(app)
+        .post("/posts")
+        .send(conejo)
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json");
+
+      expect(response.status).toBe(409);
+      expect(response.body.error).toBe("Post with this title already exists");
+    });
+  });
+});
